993b990e4ff6e640f3c1acdf70f0783b0763d648
JesusBlan <jezublanco@gmail.com>
2025-02-04 21:27:38 +0100

    created arrays to store todos and projects, added a class and a function to create todos and store them in the todos array

--- src/home.js
+++ src/home.js
@@ -1,1 +1,22 @@
-export const greeting = "its working";
+let todoList = []
+let projectList = [
+    asignedTodo=[]
+]
+
+//todo class
+class todo {
+    constructor(title, description, limitDate, priority, checked){
+        this.title = title;
+        this.description = description;
+        this.limitDate = limitDate;
+        this.priority = priority;
+        this.checked = checked;
+    }
+}
+
+//function to create new todo using the class
+function addTodo(title, description, limitDate, priority, checked){
+    let newTodo = new todo(title, description, limitDate, priority, checked);
+    todoList.push(newTodo);
+    
+}
