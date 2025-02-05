ef8006bb5658fa25a89d65ab5e7b96e2d6ede172
JesusBlan <jezublanco@gmail.com>
2025-02-05 13:08:54 +0100

    added a class and a function to create projects

--- src/home.js
+++ src/home.js
@@ -1,7 +1,7 @@
 let todoList = []
 let projectList = []
 
-//todo class
+//todos
 class todo {
     constructor(title, description, limitDate, priority, checked){
         this.title = title;
@@ -12,7 +12,7 @@ class todo {
     }
 }
 
-//function to create new todo using the class
+
 function addTodo(title, description, limitDate, priority, checked){
     let newTodo = new todo(title, description, limitDate, priority, checked);
     todoList.push(newTodo);
@@ -21,4 +21,22 @@ function addTodo(title, description, limitDate, priority, checked){
 addTodo("titulazo", "ta guapo", "pendiente", "urgente", false);
 addTodo("titulazo", "ta guapo", "pendiente", "urgente", false);
 addTodo("titulazo", "ta guapo", "pendiente", "urgente", false);
+
+//projects
+class project {
+    constructor(title, limit){
+        this.title = title;
+        this.limit = limit;
+        this.todo = [];
+    }
+}
+
+function addProject(title, limit){
+    let newProject = new project(title, limit);
+    projectList.push(newProject)
+}
+
+
+//
+
 export {todoList}
